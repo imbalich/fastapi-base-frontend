2bede7218037727453dbee4ec61db5717126fd06
imbalich <597987319@qq.com>
2024-12-23 22:21:46 +0800

    清除国际化配置

--- src/views/dashboard/workplace/locale/en-US.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  'menu.dashboard.workplace': 'Workplace',
-  'workplace.welcome': 'Welcome!',
-  'workplace.onlineContent': 'Online Content',
-  'workplace.putIn': 'Put In',
-  'workplace.newDay': 'Daily Additional Comments',
-  'workplace.newFromYesterday': 'New From Yesterday',
-  'workplace.pecs': 'pecs',
-};
